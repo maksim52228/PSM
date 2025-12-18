a0cd40410a516306476603db0e2397565e0d3dd2
maksim52228 <maksim52228@example.com>
2025-12-18 16:28:15 +0700

    астроено хранение медиа в Backblaze B2

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 import os
 from pathlib import Path
+from decouple import config
 
 from django.conf.global_settings import AUTHENTICATION_BACKENDS
 
@@ -150,8 +151,22 @@ CHANNEL_LAYERS = {
         },
     },
 }
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# Media files
+if DEBUG:
+    MEDIA_URL = '/media/'
+    MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+else:
+    # Use Backblaze B2 for media in production
+    AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
+    AWS_STORAGE_BUCKET_NAME = 'psm-media'  # ← замени на имя твоего bucket
+    AWS_S3_ENDPOINT_URL = 'https://s3.us-east-004.backblazeb2.com'   # ← замени на свой регион!
+    AWS_S3_REGION_NAME = ''
+    AWS_DEFAULT_ACL = 'public-read'
+    AWS_QUERYSTRING_AUTH = False
+
+    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    MEDIA_URL = f'https://{AWS_STORAGE_BUCKET_NAME}.s3.us-west-004.backblazeb2.com/'
 AUTHENTICATION_BACKENDS = [
     'axes.backends.AxesBackend',
     'django.contrib.auth.backends.ModelBackend',

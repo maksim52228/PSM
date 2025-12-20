4176288e9d57da24626752c9cc35869edc5b2b6a
maksim52228 <maksim52228@example.com>
2025-12-20 13:38:12 +0700

    поправил

--- mysite/settings.py
+++ mysite/settings.py
@@ -147,29 +147,32 @@ ADMIN_EMAIL = 'i-max8@yandex.ru'  # Для системных уведомлен
 ASGI_APPLICATION = 'mysite.asgi.application'
 
 # Media files
+# Static files (CSS, JS, Images)
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
+# Media files
 if DEBUG:
     MEDIA_URL = '/media/'
     MEDIA_ROOT = BASE_DIR / 'media'
 else:
-    # Use Backblaze B2 for static files in production
+    # Use Backblaze B2 for static and media files in production
     AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
     AWS_STORAGE_BUCKET_NAME = 'psm-media'  # ← замени на имя твоего bucket
-    AWS_S3_ENDPOINT_URL = 'https://s3.us-east-005.backblazeb2.com'
+    AWS_S3_ENDPOINT_URL = 'https://s3.us-east-005.backblazeb2.com'  # Убрал лишние пробелы
     AWS_S3_REGION_NAME = ''
     AWS_DEFAULT_ACL = 'public-read'
     AWS_QUERYSTRING_AUTH = False
 
     # Для статики
     STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-    STATIC_URL = f'https://{AWS_STORAGE_BUCKET_NAME}.s3.us-east-005.backblazeb2.com/'
+    STATIC_URL = f'https://{AWS_STORAGE_BUCKET_NAME}.s3.us-east-005.backblazeb2.com/static/'
 
     # Для медиа
     DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-    MEDIA_URL = f'https://{AWS_STORAGE_BUCKET_NAME}.s3.us-east-005.backblazeb2.com/'
+    MEDIA_URL = f'https://{AWS_STORAGE_BUCKET_NAME}.s3.us-east-005.backblazeb2.com/media/'
+
 AUTHENTICATION_BACKENDS = [
     'axes.backends.AxesBackend',
     'django.contrib.auth.backends.ModelBackend',
